8e328ae2f4868633180b913fb0eec4b6a7bacfd4
Aydex <adrian.hundseth@hotmail.com>
2014-09-10 16:26:07 +0200

    working øving3

--- Oving3/oving3.py
+++ Oving3/oving3.py
@@ -10,26 +10,44 @@ class Node:
 
 def bygg(ordliste):
     toppnode = Node()
-    for ord in ordliste:
-        print ord[0]
-        print ord[1]
-        tempnode = Node()
-        #if toppnode.barn.has_key(ord[0]):
-            #toppnode.barn.update(ord[0], (toppnode.barn[ord[0]], toppnode.)
-        toppnode.barn[ord[0]] = tempnode
-        for letter in ord:
-            pass
+    for ord in ordliste.keys():
+        tempnode = toppnode
+        last = ord[-1]
+        for i in ord:
+            if i not in tempnode.barn.keys():
+                node = Node()
+                tempnode.barn[i] = node
+                if i == last:
+                    node.posi.extend(ordliste.get(ord))
+                tempnode = node
+            elif i == last:
+                tempnode.barn[i].posi.extend(ordliste.get(ord))
+            else:
+                tempnode = tempnode.barn[i]
+    return toppnode
 
 
 def posisjoner(ord, indeks, node):
-    pass
+    retlist = []
+    if indeks == len(ord):
+        retlist.extend(node.posi)
+        return retlist
+    if ord[indeks] == '?':
+        for node in node.barn.values():
+            retlist.extend(posisjoner(ord, indeks + 1, node))
+    if ord[indeks] in node.barn.keys():
+        retlist.extend(posisjoner(ord, indeks + 1, node.barn[ord[indeks]]))
+    return retlist
 
 try:
     ord = stdin.readline().split()
-    ordliste = []
+    ordliste = {}
     pos = 0
     for o in ord:
-        ordliste.append( (o, pos) )
+        if o not in ordliste:
+            ordliste[o] = [pos]
+        else:
+            ordliste[o].append(pos)
         pos += len(o) + 1
     toppnode = bygg(ordliste)
     for sokeord in stdin:
