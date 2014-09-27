95222278ae3bd9b1e0bdafc543d713edf3d4430e
Aydex <adrian.hundseth@hotmail.com>
2014-09-27 16:38:48 +0200

    completed øving 5

--- Oving5/oving5.py
+++ Oving5/oving5.py
@@ -1,22 +1,44 @@
 __author__ = 'adrianh'
 from sys import stdin
+from heapq import *
 
-Inf = float(1e3000)
+
+def Inf():
+    return float(1e3000)
 
 
 def mst(nm):
-    return
+    heap = []
+    visited = []
+    high = 0
+    current_node = 0
+    for e in xrange(0, len(nm[current_node])):
+        current_weight = nm[current_node][e]
+        if current_weight < Inf() and e not in visited:
+            heappush(heap, (current_weight, current_node, e))
+    while heap:
+        current_edge = heappop(heap)
+        visited.append(nm[current_edge[1]])
+        current_node = current_edge[2]
+        root = current_edge[0]
+        for e in xrange(0, len(nm[current_node])):
+            current_weight = nm[current_node][e]
+            if current_weight < Inf() and nm[current_node] not in visited:
+                heappush(heap, (current_weight, current_node, e))
+                if root > high:
+                    high = root
+    return high
 
 
 def main():
     linjer = []
-    for str in stdin:
-        linjer.append(str)
+    for line in stdin:
+        linjer.append(line)
     n = len(linjer)
     nabomatrise = [None] * n
     node = 0
     for linje in linjer:
-        nabomatrise[node] = [Inf] * n
+        nabomatrise[node] = [Inf()] * n
         for k in linje.split():
             data = k.split(':')
             nabo = int(data[0])
